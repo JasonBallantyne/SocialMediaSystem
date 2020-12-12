49da9fe71adc5c38bad483311e38bc42fdc09de5
Jason Ballantyne <jason.ballantyne@ucdconnect.ie>
2020-12-12 18:24:37 +0000

    Added a new user

--- create.sh
+++ create.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Error: parameters problem">&2
+	exit 1
+fi
+user= "$1"
+if [ -d "$user" ]; then
+	echo "Error: user already exists">&2
+	exit 2
+elif [ ! -d "$user" ]; then
+	mkdir "user" && touch "$user"/wall "$user"/friends
+	echo "Ok: user created"
+	exit 0
+fi
