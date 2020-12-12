e26e27be64dbb5254311840aa445f9f268d32725
Jason Ballantyne <jason.ballantyne@ucdconnect.ie>
2020-12-12 20:49:02 +0000

    Adding a new friend

--- add.sh
+++ add.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+user=$1
+friend=$2
+
+if [ "$#" -ne 2 ]; then
+        echo "There is an error with this entry." >&2
+        exit 1
+
+elif ! [ -d "$user" ]; then
+        echo "This user does not exist on the system." >&2
+        exit 2
+elif ! [ -d "$friend" ]; then
+	echo "This friend does not exist on the system." >&2
+	exit 3
+elif grep -Fxp "$friend" "$user/friends"; then
+	echo "$friend and $user are already friends." >&2
+	exit 4
+else
+	echo "$friend" >> "$user/friends"
+	echo "The friend has been added successfully"
+	exit 0
+fi
