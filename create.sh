29f6299eb54367da380c2249e1cb3516712e4fd2
Jason Ballantyne <jason.ballantyne@ucdconnect.ie>
2020-12-17 22:13:39 +0000

    Semaphores Added

--- create.sh
+++ create.sh
@@ -5,11 +5,14 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 user="$1"
+./P.sh "$user"
 if [ -d "$user" ]; then
 	echo "Error: user already exists" >&2
+	./V.sh "$user"
 	exit 2
 elif [ ! -d "$user" ]; then
 	mkdir "$user" && touch "$user"/wall "$user"/friends
 	echo "OK: user created"
-	exit 0
 fi
+./V.sh "$user"
+exit 0
