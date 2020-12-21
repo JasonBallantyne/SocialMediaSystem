969fef5bcbe59c50ad01aba2040d0e5255e8ecee
Jason Ballantyne <jason.ballantyne@ucdconnect.ie>
2020-12-21 21:24:22 +0000

    Addition of the Client

--- client.sh
+++ client.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
 
-clientId="$1"
+id="$1"
 req="$2"
+user="$3"
+friend="$4"
+message="$5"
+
 
 if [ "$#" -lt 2 ]; then
 	echo "Error: parameters problem" >&2
 	exit 1
+fi
+
+clientpipe="/home/cs13432788/project-JasonBallantyne/$id.pipe"
+
+mkfifo "$clientpipe"
+
+serverpipe="/home/cs13432788/project-JasonBallantyne/server.pipe"
+
+if [ "$req" = create ]; then
+	echo "$id $req $user $friend $message">$serverpipe
+
+elif [[ "$req" = add ]]; then
+	echo "$id $req $user $friend $message">$serverpipe
+
+elif [[ "$req" = post ]]; then
+	echo "$id $req $user $friend $message">$serverpipe
+
+elif [[ "$req" = show ]]; then
+        echo "$id $req $user $friend $message">$serverpipe
+
+elif [[ "$req" = shutdown ]]; then
+	echo "$id $req">$serverpipe
+
+fi
+
+while read output; do
+	echo "$output"
+
+done<"$clientpipe"
+rm "$clientpipe"
 
--- server.sh
+++ server.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 # Error checking is done within the respective script it calls
 
-mkfifo server.pipe
+serverpipe="/home/cs13432788/project-JasonBallantyne/server.pipe"
+
+mkfifo $serverpipe
 
 while true; do
-	read -a request < server.pipe
+	read -a request < $serverpipe
 
+clientpipe="/home/cs13432788/project-JasonBallantyne/${request[0]}.pipe"
 
-	case "$request" in
+	case "${request[1]}" in
 		create)
-			./create.sh "${arr[@]:1}"
+			./create.sh "${request[@]:2}" &>"$clientpipe"
 			;;
 		add)
-			./add.sh "${arr[@]:1}"
+			./add.sh "${request[@]:2}" &>"$clientpipe"
 			;;
 		post)
-			./post.sh "${arr[@]:1}"
+			./post.sh "${request[@]:2}" &>"$clientpipe"
 			;;
 		show)
-			./show.sh "${arr[@]:1}"
+			./show.sh "${request[@]:2}" &>"$clientpipe"
 			;;
 		shutdown)
+			echo "Shutting down..." &>"$clientpipe"
+			rm $serverpipe
 			exit 0
 			;;
 		*)
 			echo "Error: bad request"
+			rm $serverpipe
 			exit 1
 	esac
+
 done
+rm $serverpipe
