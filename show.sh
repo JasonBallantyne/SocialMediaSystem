d2668de726e1bbce1a3c21e89996dd8257da2e28
Jason Ballantyne <jason.ballantyne@ucdconnect.ie>
2020-12-23 17:27:31 +0000

    Final Submission

--- show.sh
+++ show.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 user=$1
-
+./P.sh "$user"
 if [ $# -ne 1 ]; then
         echo "Error: parameters problem" >&2
+	./V.sh "$user"
         exit 1
 elif ! [ -d "$user" ]; then
         echo "$user does not exist" >&2
+	./V.sh "$user"
         exit 2
 else
         echo "wallStart"
 	echo "$(<"$user"/wall)"
 	echo "wallEnd"
+	./V.sh "$user"
         exit 0
 fi
